53ccd9d158785fb78e0b29949aa8e9b6633ef221
PalomaRmMz <git config --global user.email>
2025-01-25 14:46:11 -0600

    en servidor modificado

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const catalogosRoutes = require("./routes/catalogos");
 const usuariosRoutes = require("./routes/usuarios");
 const visitasRoutes = require("./routes/visitas");
+const visitasFilterRoutes = require("./routes/visitasFilter");
 const visitantesRoutes = require("./routes/visitantes");
-const cat_colonias = require("./routes/cat_colonias");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/catalogos", catalogosRoutes);
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/visitas", visitasRoutes);
+app.use("/api/visitas/filtro", visitasFilterRoutes);
 app.use("/api/visitantes", visitantesRoutes);
-app.use("/api/cat_colonias", cat_colonias);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
