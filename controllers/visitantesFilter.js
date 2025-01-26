10598a35f97d652c1e569a3779188026cebb620c
PalomaRmMz <git config --global user.email>
2025-01-26 13:43:40 -0600

    visitantes corregidos

--- controllers/visitantesFilter.js
+++ controllers/visitantesFilter.js
@@ -1,8 +1,8 @@
 const db = require("../config/db");
-const { getVisitasBaseQuery } = require("../queries/visitas");
+const { getVisitantesBaseQuery } = require("../queries/visitantes");
 
 const getVisitantesFilter = (filters, callback) => {
-  let query = getVisitasBaseQuery;
+  let query = getVisitantesBaseQuery;
 
   const conditions = [];
   const values = [];
